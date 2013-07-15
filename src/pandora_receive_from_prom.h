efc3f6c485b6ae3fd2a86b2b7f9f9baf36885701
nilsbeau <nilsbeau@fd298be7-36cc-4ec4-a0e8-09baf7357fce>
2013-07-15 15:45:21 +0000

    - oublie
    git-svn-id: svn+ssh://cvs-etis.ensea.fr/svn/SimulateurV1/pandora/trunk@37 fd298be7-36cc-4ec4-a0e8-09baf7357fce

---
+++ src/pandora_receive_from_prom.h
@@ -0,0 +1,31 @@
+/**
+ * pandora_receive_from_prom.h
+ *
+ *  Created on: 15 juil. 2013
+ *      Author: Nils Beaussé
+ */
+
+#ifndef PANDORA_RECEIVE_FROM_PROM_H_
+#define PANDORA_RECEIVE_FROM_PROM_H_
+
+#include "common.h"
+#include "pandora.h" // pour les type_script group etc...
+
+
+#define diff(a,b) (a > b ? a-b : b-a)
+#define permut(a,b,tmp) {tmp = a; a = b; b = tmp;}
+
+/* "En-tête" de variables globales */
+//extern ENetHost* enet_server;
+extern pthread_t enet_thread;
+
+/* En-tête de fonctions */
+void server_for_promethes();
+void enet_manager(ENetHost *server);
+void verify_script(type_script *script);
+void verify_group(type_group *group);
+void sort_list_groups_by_rate(type_group **groups, int number_of_groups);
+
+
+
+#endif /* PANDORA_RECEIVE_FROM_PROM_H_ */
